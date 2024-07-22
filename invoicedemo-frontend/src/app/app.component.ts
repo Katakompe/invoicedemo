96b0a3e0a72d517b73ddf0fde86ba80f2fcdeebb
Sebastian K <23136132+Katakompe@users.noreply.github.com>
2024-07-22 19:26:49 +0200

    Implements invoice table and api client

--- invoicedemo-frontend/src/app/app.component.ts
+++ invoicedemo-frontend/src/app/app.component.ts
@@ -1,13 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {ApiModule, BASE_PATH, InvoiceListItem, InvoiceService} from "./core/modules/openapi";
+import {SERVER_BASE_URL} from "./env";
+import {Observable, of} from "rxjs";
+import {MatTableModule} from "@angular/material/table";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ApiModule, MatTableModule],
+  providers: [
+    {provide: BASE_PATH, useValue: SERVER_BASE_URL},
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'invoicedemo-frontend';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['invoiceNumber', 'billingAmount', 'dueDate']
+
+  invoices$: Observable<InvoiceListItem[]> = of();
+
+  constructor(private invoiceService: InvoiceService) {
+  }
+
+  ngOnInit() {
+    this.loadInvoices();
+  }
+
+  private loadInvoices() {
+    this.invoices$ = this.invoiceService.invoicesGet();
+  }
 }
---
+++ invoicedemo-frontend/src/app/env.ts
@@ -0,0 +1,6 @@
+/**
+ * File containing relevant environment constants, such as server URL.
+ */
+
+export const SERVER_BASE_URL: String = 'localhost:8080'
+
