3452c61f657e28b96e6ff750e5fdac709402954a
Sebastian K <23136132+Katakompe@users.noreply.github.com>
2024-07-23 18:01:34 +0200

    Adds Angular tests

--- invoicedemo-frontend/src/app/app.component.spec.ts
+++ invoicedemo-frontend/src/app/app.component.spec.ts
@@ -1,22 +1,32 @@
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {ApiModule} from './generated/modules/openapi';
 import {provideHttpClient} from '@angular/common/http';
 import {HttpTestingController, provideHttpClientTesting} from '@angular/common/http/testing';
+import {of} from "rxjs";
+import {MatTableModule} from "@angular/material/table";
+import {HarnessLoader} from "@angular/cdk/testing";
+import {TestbedHarnessEnvironment} from "@angular/cdk/testing/testbed";
+import {MatTableHarness} from "@angular/material/table/testing";
+
+let loader: HarnessLoader;
+let fixture: ComponentFixture<AppComponent>;
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, ApiModule],
+      imports: [AppComponent, ApiModule, MatTableModule],
       providers: [provideHttpClient(), provideHttpClientTesting()]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -25,6 +35,7 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('h1')?.textContent).toContain('Your invoices');
   });
 
+
   it('should call /invoices', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
@@ -32,19 +43,33 @@ describe('AppComponent', () => {
     httpTesting.expectOne('http://localhost:8080/invoices', 'Request to load the invoice table');
   });
 
-  it('should have a table with 2 entries', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  it('should have a table with 2 entries', async () => {
+    const table = await loader.getHarness(MatTableHarness);
+    fixture.componentInstance.invoices$ = of([
+        {invoiceNumber: "AB1", dueDate: "2023-03-03", billingAmount: 22.22},
+        {invoiceNumber: "AB2", dueDate: "2023-03-03", billingAmount: 10000.99}
+      ]
+    );
     fixture.detectChanges();
-    let httpTesting = TestBed.inject(HttpTestingController);
-    const req = httpTesting.expectOne('http://localhost:8080/invoices', 'Request to load the invoice table');
+    const rows = await table.getRows();
+    expect(rows.length).toBe(2);
+  });
 
-    req.flush('[' +
-      '{"invoiceNumber":"AB1","dueDate":[2024,1,23],"billingAmount":22.22},' +
-      '{"invoiceNumber":"AB2","dueDate":[2024,12,17],"billingAmount":10000.99}' +
-      ']');
+  it('should open MatDialog on click', async () => {
+    const table = await loader.getHarness(MatTableHarness);
+    fixture.componentInstance.invoices$ = of([
+        {invoiceNumber: "AB1", dueDate: "2023-03-03", billingAmount: 22.22},
+        {invoiceNumber: "AB2", dueDate: "2023-03-03", billingAmount: 10000.99}
+      ]
+    );
 
-    const compiled = fixture.nativeElement as HTMLElement;
-    //compiled.querySelectorAll('.mat-row').forEach(v => console.log(v))
-    expect(compiled.querySelectorAll('tr.mat-mdc-row').length).toBe(2)
+    fixture.detectChanges();
+    const rows = await table.getRows();
+    const testelement = await rows[0].host();
+    await testelement.click();
+
+    expect(fixture.componentInstance.dialog.openDialogs.length).toBe(1);
+
   });
 });
--- invoicedemo-frontend/src/app/invoice-detail-dialog/invoice-detail-dialog.component.spec.ts
+++ invoicedemo-frontend/src/app/invoice-detail-dialog/invoice-detail-dialog.component.spec.ts
@@ -1,9 +1,17 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {InvoiceDetailDialogComponent} from './invoice-detail-dialog.component';
-import {ApiModule} from "../generated/modules/openapi";
+import {ApiModule, Invoice} from "../generated/modules/openapi";
 import {provideHttpClient} from "@angular/common/http";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {HttpTestingController, provideHttpClientTesting} from "@angular/common/http/testing";
+import {HarnessLoader} from "@angular/cdk/testing";
+import {MatCardModule} from "@angular/material/card";
+import {TestbedHarnessEnvironment} from "@angular/cdk/testing/testbed";
+import {MatCardHarness} from "@angular/material/card/testing";
+
+let loader: HarnessLoader;
+let fixture: ComponentFixture<InvoiceDetailDialogComponent>;
 
 describe('InvoiceDetailDialogComponent', () => {
   let component: InvoiceDetailDialogComponent;
@@ -11,18 +19,53 @@ describe('InvoiceDetailDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InvoiceDetailDialogComponent, ApiModule],
-      providers: [provideHttpClient(),
-        { provide: MatDialogRef, useValue: {} }, { provide: MAT_DIALOG_DATA, useValue: "A1" }]
+      imports: [InvoiceDetailDialogComponent, ApiModule, MatCardModule],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {provide: MatDialogRef, useValue: {}}, {provide: MAT_DIALOG_DATA, useValue: "A1"}]
     })
-    .compileComponents();
-
+      .compileComponents();
     fixture = TestBed.createComponent(InvoiceDetailDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
+
+  it('should render title', async () => {
+    fixture.componentInstance.invoice = {address: "", name: "", billingAmount: 0, dueDate: "", invoiceNumber: ""};
+    fixture.detectChanges()
+    const matCard = await loader.getHarness(MatCardHarness);
+    const title = await matCard.getTitleText()
+    expect(title).toBe("Invoice A1");
+  });
+
+  it('should call /invoice/A1', () => {
+    const fixture = TestBed.createComponent(InvoiceDetailDialogComponent);
+    fixture.detectChanges();
+    let httpTesting = TestBed.inject(HttpTestingController);
+    httpTesting.expectOne('http://localhost:8080/invoice/A1', 'Request to load the invoice with number A1');
+  });
+
+  it('should render all invoice details', () => {
+    const invoice: Invoice = {
+      invoiceNumber: "AB1",
+      dueDate: "2023-03-03",
+      billingAmount: 22.22,
+      name: "Bill",
+      address: "mystreet 1"
+    };
+    fixture.componentInstance.invoice = invoice
+    const numKeys = Object.keys(invoice).length
+
+    fixture.detectChanges()
+
+    const nativeElement = fixture.nativeElement as HTMLElement;
+    const numRows = nativeElement.querySelectorAll('tr').length
+    expect(numRows).toBe(numKeys);
+  });
+
 });
